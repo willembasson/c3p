81cc06bf026aa6c5c0a2ca2cff56378b17910ad2
wbasson <willem.basson@zoopla.co.uk>
2023-05-07 19:10:15 +0100

    input and output of different kinds

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use clap::{Parser, Subcommand};
+use atty::{is, Stream};
+use clap::Parser;
 use std::path::PathBuf;
+use url::Url;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional input to operate on (could also pipe from StdIn)
+    /// Input to operate on (Use - if you pipe from StdIn)
     input: Option<String>,
 
     /// Sets a custom config file
@@ -15,21 +17,94 @@ struct Cli {
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
 
-    /// Optional output to operate on (could also pipe to StdOut)
+    /// Output to operate on (Use - to pipe to StdOut)
     output: Option<String>,
-
-    #[command(subcommand)]
-    command: Option<Commands>,
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
-    },
+#[derive(Debug)]
+enum InputKind {
+    OrdinaryFile(PathBuf),
+    StdIn,
+    Url(url::Url),
+    S3Bucket(String),
+}
+
+#[derive(Debug)]
+struct Input {
+    kind: InputKind,
+}
+
+fn to_input(input: String) -> Input {
+    let input_url = Url::parse(input.as_str());
+    if input_url.is_ok() {
+        if input.starts_with("s3://") {
+            println!("S3 !!");
+            Input {
+                kind: InputKind::S3Bucket(input),
+            }
+        } else {
+            println!("Probably a url");
+            Input {
+                kind: InputKind::Url(input_url.unwrap()),
+            }
+        }
+    } else if input == "-" {
+        if is(Stream::Stdin) {
+            println!("You said StdIn but you didn't pipe or redirect anything");
+        }
+        println!("Std In");
+        Input {
+            kind: InputKind::StdIn,
+        }
+    } else {
+        println!("Probabaly a file");
+        Input {
+            kind: InputKind::OrdinaryFile(PathBuf::from(input)),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum OutputKind {
+    OrdinaryFile(PathBuf),
+    StdOut,
+    Url(url::Url),
+    S3Bucket(String),
+}
+
+#[derive(Debug)]
+struct Output {
+    kind: OutputKind,
+}
+
+fn to_output(output: String) -> Output {
+    let output_url = Url::parse(output.as_str());
+    if output_url.is_ok() {
+        if output.starts_with("s3://") {
+            println!("S3 !!");
+            Output {
+                kind: OutputKind::S3Bucket(output),
+            }
+        } else {
+            println!("Probably a url");
+            Output {
+                kind: OutputKind::Url(output_url.unwrap()),
+            }
+        }
+    } else if output == "-" {
+        if is(Stream::Stdout) {
+            println!("You said StdOut but you didn't pipe or redirect anything");
+        }
+        println!("Std Out");
+        Output {
+            kind: OutputKind::StdOut,
+        }
+    } else {
+        println!("Probabaly a file");
+        Output {
+            kind: OutputKind::OrdinaryFile(PathBuf::from(output)),
+        }
+    }
 }
 
 fn main() {
@@ -37,32 +112,16 @@ fn main() {
 
     // You can check the value provided by positional arguments, or option arguments
     if let Some(input) = cli.input.as_deref() {
-        println!("Value for input: {input}");
+        let input_string = input.to_string();
+        let input: Input = to_input(input_string);
+        println!("{:#?}", input);
     }
-
+    if let Some(output) = cli.output.as_deref() {
+        let output_string = output.to_string();
+        let output: Output = to_output(output_string);
+        println!("{:#?}", output);
+    }
     if let Some(config_path) = cli.config.as_deref() {
         println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
     }
 }
